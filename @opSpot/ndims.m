fe503d50e3bc03e357d273e2a55b338a1177c8fb
ewout78 <ewout78@602325e8-326d-0410-a6db-8800ab266cdc>
2009-06-27 06:35:38 +0000

    spot: Added uplus and ndims
    git-svn-id: svn://tuatara.cs.ubc.ca/spot/trunk@9 602325e8-326d-0410-a6db-8800ab266cdc

---
+++ @opSpot/ndims.m
@@ -0,0 +1,8 @@
+function n = ndims(op)
+%NDIMS  Number of dimensions.
+
+%   Copyright 2009, Ewout van den Berg and Michael P. Friedlander
+%   http://www.cs.ubc.ca/labs/scl/sparco
+%   $Id: ndims.m 39 2009-06-12 20:59:05Z ewout78 $
+
+n = 2;
