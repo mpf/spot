855a84f3dd442965afcc99f8ff7299a846f6bb1a
kyc <kyc@602325e8-326d-0410-a6db-8800ab266cdc>
2009-07-17 00:19:56 +0000

    updated test_char, and formatted output for better visual
    git-svn-id: svn://tuatara.cs.ubc.ca/spot/trunk@36 602325e8-326d-0410-a6db-8800ab266cdc

--- tests/templates/test_Char.m
+++ tests/templates/test_Char.m
@@ -1,6 +1,9 @@
 function test_Char(data)
 
 A = data.operator;
-char(A);
+opType = data.operatorType;
+
+[m,n]=size(A);
+assertEqual(char(A),[opType,'(',num2str(m),',',num2str(n),')']);
 
 end
--- tests/templates/test_NormEst.m
+++ tests/templates/test_NormEst.m
@@ -2,7 +2,8 @@ function test_NormEst(data)
 %btNormEst(A, tol) Unit test for normest
 %this test assumes double is implemented correctly.
 A=data.operator;
-tol=data.relativeTol;
+%tol=data.relativeTol;
+tol = 1e-5;
 
 a = normest(A);
 b = normest(double(A));
