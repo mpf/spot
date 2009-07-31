f809c2b07ae2c04a418c9d32205b10f9ad21d7a8
Michael P. Friedlander <mpf@cs.ubc.ca>
2009-07-31 15:24:14 -0700

    New test for opOrthogonal.

---
+++ tests/test_opOrthogonal.m
@@ -0,0 +1,32 @@
+function test_suite = test_opOrthogonal
+%test_opOrthogonal  Unit tests for opOrthogonal and derivative operators
+
+function test_opOrthogonal
+   
+   n = 23; % whatever...
+   
+   Q = opDCT(n); b = Q.rrandn;
+   assertEqual( Q\b, Q'*b )
+   assertElementsAlmostEqual( svd(double(Q)), ones(size(Q,1),1) );
+
+   Q = opDCT2(n); b = Q.rrandn;
+   assertEqual( Q\b, Q'*b )
+   assertElementsAlmostEqual( svd(double(Q)), ones(size(Q,1),1) );
+
+   Q = opDFT(n);  b = Q.rrandn;
+   assertEqual( Q\b, Q'*b )
+   assertElementsAlmostEqual( svd(double(Q)), ones(size(Q,1),1) );
+   
+   Q = opDFT2(n);  b = Q.rrandn;
+   assertEqual( Q\b, Q'*b )
+   assertElementsAlmostEqual( svd(double(Q)), ones(size(Q,1),1) );
+
+   Q = opDirac(n);  b = Q.rrandn;
+   assertEqual( Q\b, Q'*b )
+   assertElementsAlmostEqual( svd(double(Q)), ones(size(Q,1),1) );
+
+   Q = opHaar(128);  b = Q.rrandn;
+   assertEqual( Q\b, Q'*b )
+   assertElementsAlmostEqual( svd(double(Q)), ones(size(Q,1),1) );
+   
+end
