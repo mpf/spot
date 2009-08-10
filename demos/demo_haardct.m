161e4ec2a2ace42a3ea95d15efcc1a2f4aa30c30
Ewout van den Berg <ewout78@cs.ubc.ca>
2009-08-10 15:05:29 -0700

    Added initial version of demo_haardct.m

---
+++ demos/demo_haardct.m
@@ -0,0 +1,32 @@
+%% Interpolation of partially sampled DCT-Haar signal
+
+
+%% Create synthesis operators
+D = opDCT(512);
+H = opHaar(512,8);
+
+%% Create sparse signal
+rand('state',0); randn('state',3); % 0
+p = randperm(48);
+xd = zeros(512,1); xd(p(1:5)) = randn(5,1);
+p = randperm(10);
+xh = zeros(512,1); xh(p(1:5)) = randn(5,1);
+signalD = D' * xd;
+signalH = H' * xh;
+signal  = signalD + signalH;
+
+%% Create synthesis dictionary
+B = [D',H'];
+
+%% Sample at random locations
+rand('state',1);
+p = randperm(512);
+idx = sort(p(1:85));
+plot(1:length(signal),signal,'b-',idx,signal(idx),'r*');
+
+R = opRestriction(512,idx);
+A = R*B; % A = B(idx,:);
+x = spg_bp(A,signal(idx));
+
+hold on; plot(B*x,'r--'); hold off;
+
