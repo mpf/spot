38d1c7ad2ebc885b6cc6acce851201d3f1027e37
Michael Friedlander <mpf@cs.ubc.ca>
2010-08-12 15:39:32 -0700

    Before check, convert to full.

--- tests/test_sparsevecs.m
+++ tests/test_sparsevecs.m
@@ -10,36 +10,36 @@ randn('state',0); rand('state',0)
 m = randi(100);
 n = randi(100);
 
-[A,xs,xf] = generate(opBernoulli(m,n));  assertEqual( A*xs, A*xf );
-[A,xs,xf] = generate(opBinary(m,n));     assertEqual( A*xs, A*xf );
-[A,xs,xf] = generate(opDCT(n));          assertEqual( A*xs, A*xf );
-[A,xs,xf] = generate(opDCT2(m,n));       assertEqual( A*xs, A*xf );
-[A,xs,xf] = generate(opDirac(n));        assertEqual( A*xs, A*xf );
-[A,xs,xf] = generate(opEmpty(m,0));      assertEqual( A*xs, A*xf );
-[A,xs,xf] = generate(opEye(n));          assertEqual( A*xs, A*xf );
-[A,xs,xf] = generate(opDFT(n));          assertEqual( A*xs, A*xf );
-[A,xs,xf] = generate(opDFT2(m,n));       assertEqual( A*xs, A*xf );
-[A,xs,xf] = generate(opGaussian(m,n));   assertEqual( A*xs, A*xf );
-[A,xs,xf] = generate(opHaar(64));        assertEqual( A*xs, A*xf );
-[A,xs,xf] = generate(opHaar(64,5,1));    assertEqual( A*xs, A*xf );
-[A,xs,xf] = generate(opHaar2(64,128));   assertEqual( A*xs, A*xf );
-[A,xs,xf] = generate(opHadamard(256));   assertEqual( A*xs, A*xf );
-[A,xs,xf] = generate(opHadamard(256,1)); assertEqual( A*xs, A*xf );
-[A,xs,xf] = generate(opHeaviside(n));    assertEqual( A*xs, A*xf );
-[A,xs,xf] = generate(opHeaviside(n,1));  assertEqual( A*xs, A*xf );
-[A,xs,xf] = generate(opOnes(m,n));       assertEqual( A*xs, A*xf );
+[A,xs,xf] = generate(opBernoulli(m,n));  assertEqual( full(A*xs), A*xf );
+[A,xs,xf] = generate(opBinary(m,n));     assertEqual( full(A*xs), A*xf );
+[A,xs,xf] = generate(opDCT(n));          assertEqual( full(A*xs), A*xf );
+[A,xs,xf] = generate(opDCT2(m,n));       assertEqual( full(A*xs), A*xf );
+[A,xs,xf] = generate(opDirac(n));        assertEqual( full(A*xs), A*xf );
+[A,xs,xf] = generate(opEmpty(m,0));      assertEqual( full(A*xs), A*xf );
+[A,xs,xf] = generate(opEye(n));          assertEqual( full(A*xs), A*xf );
+[A,xs,xf] = generate(opDFT(n));          assertEqual( full(A*xs), A*xf );
+[A,xs,xf] = generate(opDFT2(m,n));       assertEqual( full(A*xs), A*xf );
+[A,xs,xf] = generate(opGaussian(m,n));   assertEqual( full(A*xs), A*xf );
+[A,xs,xf] = generate(opHaar(64));        assertEqual( full(A*xs), A*xf );
+[A,xs,xf] = generate(opHaar(64,5,1));    assertEqual( full(A*xs), A*xf );
+[A,xs,xf] = generate(opHaar2(64,128));   assertEqual( full(A*xs), A*xf );
+[A,xs,xf] = generate(opHadamard(256));   assertEqual( full(A*xs), A*xf );
+[A,xs,xf] = generate(opHadamard(256,1)); assertEqual( full(A*xs), A*xf );
+[A,xs,xf] = generate(opHeaviside(n));    assertEqual( full(A*xs), A*xf );
+[A,xs,xf] = generate(opHeaviside(n,1));  assertEqual( full(A*xs), A*xf );
+[A,xs,xf] = generate(opOnes(m,n));       assertEqual( full(A*xs), A*xf );
 
-%[A,xs,xf] = generate(opSurfacelet([m,n],1));assertEqual( A*xs, A*xf );
+%[A,xs,xf] = generate(opSurfacelet([m,n],1));assertEqual( full(A*xs), A*xf );
 
-[A,xs,xf] = generate(opToepGauss(m,n));             assertEqual( A*xs, A*xf );
-[A,xs,xf] = generate(opToepGauss(m,n,'circular'));  assertEqual( A*xs, A*xf );
-[A,xs,xf] = generate(opToepGauss(m,n,'toeplitz',1));assertEqual( A*xs, A*xf );
-[A,xs,xf] = generate(opToepGauss(m,n,'circular',1));assertEqual( A*xs, A*xf );
+[A,xs,xf] = generate(opToepGauss(m,n));             assertEqual( full(A*xs), A*xf );
+[A,xs,xf] = generate(opToepGauss(m,n,'circular'));  assertEqual( full(A*xs), A*xf );
+[A,xs,xf] = generate(opToepGauss(m,n,'toeplitz',1));assertEqual( full(A*xs), A*xf );
+[A,xs,xf] = generate(opToepGauss(m,n,'circular',1));assertEqual( full(A*xs), A*xf );
 
-[A,xs,xf] = generate(opToepSign(m,n));             assertEqual( A*xs, A*xf );
-[A,xs,xf] = generate(opToepSign(m,n,'circular'));  assertEqual( A*xs, A*xf );
-[A,xs,xf] = generate(opToepSign(m,n,'toeplitz',1));assertEqual( A*xs, A*xf );
-[A,xs,xf] = generate(opToepSign(m,n,'circular',1));assertEqual( A*xs, A*xf );
+[A,xs,xf] = generate(opToepSign(m,n));             assertEqual( full(A*xs), A*xf );
+[A,xs,xf] = generate(opToepSign(m,n,'circular'));  assertEqual( full(A*xs), A*xf );
+[A,xs,xf] = generate(opToepSign(m,n,'toeplitz',1));assertEqual( full(A*xs), A*xf );
+[A,xs,xf] = generate(opToepSign(m,n,'circular',1));assertEqual( full(A*xs), A*xf );
 
 end
 
