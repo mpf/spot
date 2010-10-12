8ef33d035601cedcdb639b82e80fa46d113d1654
Nameet Kumar <nkumar@eos.ubc.ca>
2010-10-12 14:36:40 -0700

    Added 3d Curvelet operator and utility function it requires - fdct3d_v2c to convert vector to curvelet coefficients

---
+++ opCurvelet3d.m
@@ -0,0 +1,100 @@
+classdef opCurvelet3d < opSpot
+%OPCURVELET3D  Three-dimensional curvelet operator.
+%
+%   opCurvelet(M,N,O,NBSCALES,NBANGLES) creates a three-dimensional
+%   curvelet operator for M by N by O matrices. The curvelet transform is
+%   computed using the Curvelab code.
+%
+%   The remaining two parameters are optional; NBSCALES gives the
+%   number of scales and is set to max(1,ceil(log2(min(M,N,O)) - 3)) by
+%   default, as suggested by Curvelab. NBANGLES gives the number of
+%   angles at the second coarsest level which must be a multiple of
+%   four with a minimum of 8. By default NBANGLES is set to 16.
+%
+%   See also CURVELAB.
+
+%   Nameet Kumar - Oct 2010
+%   Copyright 2009, Gilles Hennenfent, Ewout van den Berg and Michael P. Friedlander
+%   See the file COPYING.txt for full copyright information.
+%   Use the command 'spot.gpl' to locate this file.
+
+%   http://www.cs.ubc.ca/labs/scl/spot
+
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % Properties
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    properties (SetAccess = protected)
+       nbscales;
+       nbangles;
+       ac = 1;   %currently not adjustable, but put it here for view
+       hdr;          %sizes of coefficient vectors
+       cn;           %total number of coefficients
+       sz;           %size of curvelet
+       
+    end % Properties
+
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % Methods
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    methods
+
+       %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+       % Constructor
+       %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+       function op = opCurvelet3d(m,n,p,nbscales,nbangles)
+
+          if nargin < 4, nbscales = max(1,ceil(log2(min([m,n,p])) - 3)); end;
+          if nargin < 5, nbangles = 16;                              end;
+          ac = 1;  %#ok<PROP>
+          is_real = 1; % currently this is not taken as an input, maybe 
+                        % support complex later on?
+
+          % Compute length of curvelet coefficient vector
+          [tmphdr, cn] = fdct3d_sizes_mex(m,n,p,nbscales,nbangles,ac); %#ok<PROP>
+          hdr = cell(nbscales,1);
+          hdr{1} = {[tmphdr{1:3}]};
+          for i = 2:nbscales
+             j = 4 + 10*(i - 2);
+             hdr{i} = {[tmphdr{j+1:j+3}];[tmphdr{j+4:j+6}];...
+                [tmphdr{j+7:j+9}];[tmphdr{j}]};
+          end
+          
+          % Construct operator
+          op = op@opSpot('Curvelet3d', cn, m*n*p);
+          op.cflag     = ~is_real;
+          op.nbscales = nbscales;
+          op.nbangles = nbangles;
+          op.hdr = hdr;
+          op.cn = cn;
+          op.sz = [m,n,p];
+       end % Constructor
+
+    end % Methods
+       
+ 
+    methods ( Access = protected )
+       %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+       % Multiply
+       %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+       function x = multiply(op,x,mode)
+         
+          if mode == 1
+            % Analysis mode
+            x = fdct3d_forward_mex(op.sz(1),op.sz(2),op.sz(3),...
+               op.nbscales,op.nbangles,op.ac,reshape(x,op.sz));
+            x = fdct_wrapping_c2r(x);
+            x = spot.utils.fdct_c2v(x,op.cn);
+         else
+            % Synthesis mode  
+            x = spot.utils.fdct3d_v2c(x,op.hdr);
+            x = fdct_wrapping_r2c(x);
+            x = fdct3d_inverse_mex(op.sz(1),op.sz(2),op.sz(3),...
+               op.nbscales,op.nbangles,op.ac,x);
+            x = real(x);
+            x = x(:);
+         end
+       end % Multiply          
+
+    end % Methods
+   
+end % Classdef
