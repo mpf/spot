561cfdf7ba08226752f155b03e7c79e89e3958d6
Thomas <tlai@avo.eos.ubc.ca>
2011-02-17 13:11:11 -0800

    Bugfix for spottests that reflect horzcat fix

--- tests/test_opDictionary.m
+++ tests/test_opDictionary.m
@@ -9,7 +9,7 @@ function test_opDictionary_mixed
    m = 10; nA = 20; nB = 20;
    A = opGaussian(m,nA);
    B = opBernoulli(m,nB);
-   D = [ A B ];
+   D = opDictionary(A,B);
    
    x = D.drandn;
    
@@ -24,10 +24,10 @@ end
 
 function test_opDictionary_multiple
 
-   D = [ opDCT(10) opDFT(10) double(opDCT(10)) ];
+   D = opDictionary( opDCT(10), opDFT(10), double(opDCT(10)) );
    assertFalse(dottest(D))
 
-   D = [ opDCT(10) opDFT(10) [] double(opDCT(10)) ];
+   D = opDictionary( opDCT(10), opDFT(10), [], double(opDCT(10)) );
    assertFalse(dottest(D))
    assertTrue( all(size(D) == [10 30]) )
    
@@ -38,7 +38,7 @@ function test_opDictionary_double
    E = opEye(3,4);
    R = randn(3,6);
    Z = opZeros(3,1);
-   D = [G E [] R Z];
+   D = opDictionary(G, E, [], R, Z);
    
    assertEqual( double(D), [double(G), double(E), double(R), double(Z)] )
 end
