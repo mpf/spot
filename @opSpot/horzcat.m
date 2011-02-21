4c796b194fa9cde58e7501f752024bf3e2ccc6ff
Thomas <tlai@avo.eos.ubc.ca>
2011-02-21 11:23:32 -0800

    Important bugfix: Removed pSpot automation in horzcat and vertcat because it was a bad design choice

--- @opSpot/horzcat.m
+++ @opSpot/horzcat.m
@@ -3,8 +3,6 @@ function y = horzcat(varargin)
 %
 %   [A B] is the horizonal concatenation of operators A and B.
 %
-%   If Matlabpool is on, pSpot's oppDictionary will be called.
-%
 %   See also opSpot.vertcat, opDictionary.
 
 %   Copyright 2009, Ewout van den Berg and Michael P. Friedlander
@@ -13,13 +11,4 @@ function y = horzcat(varargin)
 
 %   http://www.cs.ubc.ca/labs/scl/spot
 
-try
-    if matlabpool('size') == 0
-        y = opDictionary(varargin{:});
-    else
-        y = oppDictionary(varargin{:});
-        warning('Matlabpool detected: Parallel Dictionary engaged');
-    end
-catch
-    y = opDictionary(varargin{:});
-end
+y = opDictionary(varargin{:});
--- @opSpot/vertcat.m
+++ @opSpot/vertcat.m
@@ -3,8 +3,6 @@ function y = vertcat(varargin)
 %
 %   [A; B] is the vertical concatenation of the operators A and B.
 %
-%   If Matlabpool is on, parallel stack will be called. 
-%
 %   See also opSpot.horzcat, opStack.
 
 %   Copyright 2009, Ewout van den Berg and Michael P. Friedlander
@@ -13,13 +11,4 @@ function y = vertcat(varargin)
 
 %   http://www.cs.ubc.ca/labs/scl/spot
 
-try
-    if matlabpool('size') == 0
-        y = opStack(varargin{:});
-    else
-        y = oppStack(varargin{:});
-        warning('Matlabpool detected: Parallel Stack engaged');
-    end
-catch
-    y = opStack(varargin{:});
-end
+y = opStack(varargin{:});
