2b7c6117d7fb8e1b1edcb3974f9dd0a21af9dcd7
Thomas <tlai@avo.eos.ubc.ca>
2011-03-15 16:36:15 -0700

    Implementing oppKron with data container

--- +spot/+utils/nDimsMultiply.m
+++ +spot/+utils/nDimsMultiply.m
@@ -5,13 +5,13 @@ function y = nDimsMultiply(OP,xloc)
 %   dimensions (slice) of the n-Dimensional array x across all dimensions 
 %   3 to N recursively. OP can be a Spot operator or a numerical matrix.
 
-	if ndims(xloc) == 2
+	if numel(size(xloc)) == 2
 		y = OP*xloc;
 	else
 		sizX = size(xloc);
 		idX(1 : ndims(xloc) - 1) = {':'};
 		for i = 1:sizX(end)
-			y(idX{:},i) = nDimsMultiply(OP,xloc(idX{:},i));
+			y(idX{:},i) = spot.utils.nDimsMultiply(OP,xloc(idX{:},i));
 		end
 	end
 end
