2fa0f878a1674775b3f90e65bf960b209d3ffdcd
Thomas <tlai@avo.eos.ubc.ca>
2011-03-28 16:57:38 -0700

    Cleaned up test_opRomberg

--- tests/test_opRomberg.m
+++ tests/test_opRomberg.m
@@ -1,30 +1,34 @@
-gauss      = 0;
-n          = 2000;
-r          = 20;
-
-A1         = rand(n,r)*rand(r,n );
-tic;
-% [U1,S1,V1] = pca(A1,r,0);
-toc;
-
-if gauss,
-A21        = opGaussian(n ,r);
-A22        = opGaussian(n ,r);
-A2         = A21*A22';
-else
- G21       = opRomberg([n 1]);
- inds      = randperm(n);
- idx1      = inds(1:r);
- R21       = opRestriction(n,idx1);
- A21       = R21*G21;
- G22       = opRomberg([n 1]);
- inds      = randperm(n);
- idx2      = inds(1:r);
- R22       = opRestriction(n,idx2);
- A22       = R22*G22;
- A2        = A21'*A22;
+function test_suite = test_opRomberg
+initTestSuite;
 end
 
-tic;
+function test_opRomberg_builtin
+%% Built-in test for opRomberg
+m = randi(10);
+n = randi(10);
+A = opRomberg([m n]);
+A.utest;
+A = A';
+A.utest;
+end
+
+function test_opRomberg_gaussian
+%% Felix's Test for opRomberg
+gauss     = 0;
+n         = 2000;
+r         = 20;
+A1        = rand(n,r)*rand(r,n );
+G21       = opRomberg([n 1]);
+inds      = randperm(n);
+idx1      = inds(1:r);
+R21       = opRestriction(n,idx1);
+A21       = R21*G21;
+G22       = opRomberg([n 1]);
+inds      = randperm(n);
+idx2      = inds(1:r);
+R22       = opRestriction(n,idx2);
+A22       = R22*G22;
+A2        = A21'*A22;
+
 [U2,S2,V2] = spotpca(A2,r,0);
-toc;
+end
