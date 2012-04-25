a637e6a029bf2d8c0fedaede03b1f25790ef00bf
Thomas <yuyanglai@yahoo.com>
2012-04-25 15:34:51 -0700

    Fixed double for transposed operators

--- @opSpot/double.m
+++ @opSpot/double.m
@@ -9,7 +9,8 @@ function M = double(A)
 
 %   http://www.cs.ubc.ca/labs/scl/spot
 
-if size(A,1) < size(A,2)
+if (size(A,1) < size(A,2)) ...
+    && ~isa(A, 'opTranspose') && ~isa(A, 'opCTranspose')
    M = (A'*speye(size(A,1)))';
 else
    M = A*speye(size(A,2));
