568edcfd42bb06a8dd2efe804f4633d87598bebd
Thomas <yuyanglai@yahoo.com>
2012-05-15 12:43:25 -0700

    Added test class Mist to experiment with the Fog ineffeciency mystery

---
+++ tests/Mist/fogtest.m
@@ -0,0 +1,15 @@
+clear all
+clc
+A = Fog(randn(3,3),randn(3,3));
+for i=1:13
+    tic;
+    A  = randn(3,3) * A;
+    t1 = toc;
+    sa = whos('A');
+    tic;
+    b  = A * randn(3,1);
+    t2 = toc;
+    fprintf('Atic=%f bytes=%d btic=%f\n',t1,sa.bytes,t2);
+end
+%pause
+%clear all
