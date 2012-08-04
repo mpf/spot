52dbe90d52bf799069fde85ce50c355b9150b653
Frances Russell <frussell@lime.cs.ubc.ca>
2012-08-03 17:18:42 -0700

    Automated creation of help browser files

---
+++ spotexport.m
@@ -0,0 +1,25 @@
+function spotexport(tag)
+%SPOTPUBLISH  Exports Spot to a zip file.
+
+%   Copyright 2009, Ewout van den Berg and Michael P. Friedlander
+%   See the file COPYING.txt for full copyright information.
+%   Use the command 'spot.gpl' to locate this file.
+
+%   http://www.cs.ubc.ca/labs/scl/spot
+
+   if nargin < 1 || isempty(tag)
+      error('tag required')
+   end
+
+   % Export to zip file.
+   cd(spot.path)
+   cmd = sprintf('git archive %s --prefix=spotbox-%s/ | tar -x -C /tmp',tag,tag);
+   system(cmd);
+   
+   % Create archive
+   cd(sprintf('/tmp',tag));
+   cmd = sprintf('zip -r spotbox-%s.zip spotbox-%s',tag,tag);
+   system(cmd);
+   cd(spot.path)
+      
+end % function spotexport
