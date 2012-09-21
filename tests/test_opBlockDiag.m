a4f8e1dea8c05af886ba438272960fbf8efb84f6
Michael Friedlander <mpf@cs.ubc.ca>
2012-09-21 11:48:05 -0700

    Moved dottest to utils.

--- tests/test_opBlockDiag.m
+++ tests/test_opBlockDiag.m
@@ -29,20 +29,20 @@ function test_opBlockDiag_overlap(seed)
    % row overlap
    ov = 5;
    D  = opBlockDiag(A,B,ov);
-   assertFalse(dottest(D));
+   assertFalse(spot.utils.dottest(D));
    
    % column overlap
    ov = -5;
    D  = opBlockDiag(A,B,ov);
-   assertFalse(dottest(D));
+   assertFalse(spot.utils.dottest(D));
 
    % row-anti-diag overlap
    ov = m1+1;
    D  = opBlockDiag(A,B,ov);
-   assertFalse(dottest(D));
+   assertFalse(spot.utils.dottest(D));
 
    % col-anti-diag overlap
    ov = -(m1+1);
    D  = opBlockDiag(A,B,ov);
-   assertFalse(dottest(D));
+   assertFalse(spot.utils.dottest(D));
 end
