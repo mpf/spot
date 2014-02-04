e185488c3407a5298d78085bb4f6ceb1af5077a2
Dominique Orban <dominique.orban@gmail.com>
2014-02-03 16:47:16 -0800

    New Operator opChol.
    opChol represents the inverse of a definite operator based on
    the Cholesky factorization.

---
+++ opChol.m
@@ -0,0 +1,84 @@
+classdef opChol < opFactorization
+%opCHOL  Operator representing the Cholesky factorization of a
+%        symmetric and definite matrix with optional iterative
+%        refinement. Only the lower triangle of the input matrix
+%        is referenced.
+%
+%   opChol(A) creates an operator for multiplication by the
+%   inverse of the matrix A implicitly represented by its Cholesky
+%   factorization. Optionally, iterative refinement is performed.
+%   Note that A is an explicit matrix.
+%
+%   The following attributes may be changed by the user:
+%    * nitref : the maximum number of iterative refinement steps (3)
+%    * itref_tol : iterative refinement tolerance (1.0e-8)
+%    * force_itref : force iterative refinement (false)
+%
+%   See also chol.
+%
+%   Dominique Orban <dominique.orban@gerad.ca>, 2014.
+%
+%   Copyright 2009, Ewout van den Berg and Michael P. Friedlander
+%   See the file COPYING.txt for full copyright information.
+%   Use the command 'spot.gpl' to locate this file.
+
+%   http://www.cs.ubc.ca/labs/scl/spot
+
+  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  % Properties
+  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  properties( SetAccess = private )
+    L             % Lower triangular Cholesky factor
+  end
+
+  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  % Methods - Public
+  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  methods
+
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % opChol. Constructor
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    function op = opChol(A)
+      if nargin ~= 1
+        error('Invalid number of arguments.');
+      end
+
+      [m,n] = size(A);
+
+      % Construct operator
+      op = op@opFactorization('LU', m, n);
+      B  = A;
+      if ~issparse(A)
+       B             = sparse(A);
+      end
+      op.A           = opHermitian(B);
+      op.L           = chol(B, 'lower');
+      op.Ainv        = inv(op.L') * inv(op.L);
+      op.cflag       = ~isreal(A);
+    end % function opChol
+
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % transpose
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    function opOut = transpose(op)
+       opOut = inv(op.L.') * inv(op.L);
+    end
+
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % conj
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    function opOut = conj(op)
+       opOut = inv(conj(op.L')) * inv(conj(op.L));
+    end
+
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % ctranpose
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    function opOut = ctranspose(op)
+       opOut = inv(op.L') * inv(op.L);
+    end
+
+  end % methods - public
+
+end % classdef
